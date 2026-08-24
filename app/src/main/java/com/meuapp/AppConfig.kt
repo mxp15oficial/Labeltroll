c234e315f6f83ffbb1ce6c97ccff2561c5a28176
mxp15oficial <poltroniericorreiam@gmail.com>
2026-08-24 10:22:26 -0400

    Create AppConfig.kt

---
+++ app/src/main/java/com/meuapp/AppConfig.kt
@@ -0,0 +1,6 @@
+package com.meuapp
+
+object AppConfig {
+    // O GitHub Actions vai alterar este valor de false para true quando você rodar a clonagem
+    const val ENABLE_TREASURE: Boolean = false
+}
